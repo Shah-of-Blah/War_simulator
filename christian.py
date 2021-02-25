9da9a1e71135c8d8429d0100f65497402a0bb3c6
Brady Thompson <bradythompson@Bradys-MacBook-Pro.local>
2021-02-25 11:42:59 -0600

    new feature

--- christian.py
+++ christian.py
@@ -5,9 +5,11 @@ print('a new line here')
 
 print('how about i add a new line here')
 
-print('fake user adds a line')
+print('user 2 wants to replace this line')
 print('fake user adds new line to pulled branch, after PR')
 print('fake_user adds new line, after rebase pull')
 print('user 1 makes a suggestion')
 
 # User 2 wants this line
+
+# User 2 wants to add this features to the code
---
+++ new_file.py
@@ -0,0 +1,1 @@
+# User 2 also wants to create this new file
